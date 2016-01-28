2938f1523a161cff8f654c4b5bffbe2efff1df1c
John Mazzucco <John.Mazzucco@gmail.com>
2016-01-28 11:30:27 -0500

    display textarea and submit button on Start/Root page

--- router.js
+++ router.js
@@ -3,7 +3,7 @@ function route(handle, pathname, response) {
 	console.log("About to route a request for " + pathname);
 
 	if (typeof handle[pathname] === 'function') {
-		return handle[pathname](reponse);
+		return handle[pathname](response);
 	} else {
 		console.log("No request handler found for " + pathname);
 		response.writeHead(404, {"Content-Type": "text/plain"});
